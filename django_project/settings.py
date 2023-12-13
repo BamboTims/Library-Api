eaee8b6fc0e559c4147db33c45ddd394e51be430
Bambotims <timilehinadebambo@gmail.com>
2023-12-13 21:02:08 +0100

    api complete

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,11 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "todo.apps.TodoConfig",
+    "rest_framework",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -122,3 +125,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK  = {
+    "DEFAULT_PERMISSION_CLASSES":[
+        "rest_framework.permissions.AllowAny"
+    ]
+}
+
+CORS_ALLOWED_ORIGINS = ("http://localhost:3000","http://localhost:8000")
+
+CRSF_TRUSTED_ORIGINS = ["localhost:3000"]
